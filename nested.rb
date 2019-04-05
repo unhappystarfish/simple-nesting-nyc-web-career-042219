66b5519c68938483fb40b7c747986bd94aee711c
Jake L <jakelovitz@gmail.com>
2019-04-05 21:48:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
 
    programmer_hash[:yukihiro_matsumoto] = {known_for: "ruby", languages: ["LISP", "C"]}
    
-   programmer_hash[:yukihiro_matsumoto]
+   programmer_hash
    
 end
 
